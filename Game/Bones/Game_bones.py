bd7c0e31de531d28e7b0edb9b83f452d3ab48d15
GHR <freegenrih@mail.ru>
2017-05-22 23:41:45 +0300

    reformat code Bones

--- Game/Bones/Game_bones.py
+++ Game/Bones/Game_bones.py
@@ -1,7 +1,6 @@
 from random import *
 
-
-print("***         Игра Кости           ***" )
+print("***         Игра Кости           ***")
 print('''
     *************  *************
     *           *  *           *
@@ -19,45 +18,49 @@ p1_name = str(input())
 print("*    Введите имя второго игрока    *")
 p2_name = str(input())
 
-sp_one=0
-sp_tho=0
+sp_one = 0
+sp_tho = 0
+
 
 def sumplayer(x):
-    if x==1:
+    if x == 1:
         global sp_one
-        sp_one+=1
-    elif x==2:
+        sp_one += 1
+    elif x == 2:
         global sp_tho
-        sp_tho+=1
+        sp_tho += 1
     else:
         pass
 
+
 def score():
-        print("Счет "+str(sp_one)+":"+str(sp_tho)+"\n")       
+    print("Счет " + str(sp_one) + ":" + str(sp_tho) + "\n")
+
 
 def player():
-        print("Бросает кость  "+p1_name)
-        p1 = input()
-        p1_val = randint(1,6)
-        print("Выпала кость  = "+str(p1_val)+"\n")
-        print("Бросает кость "+p2_name)
-        input()
-        p2_val = randint(1,6)
-        print("Выпала кость  = "+str(p2_val)+"\n")
-        if p1_val>p2_val:         
-            sumplayer(1)
-            print("Игрок "+p1_name+" Выиграл\n")
-            score()
-        elif p1_val==p2_val:
-            print("Ничья\n")
-            score()
-                   
-        elif p1_val<p2_val:
-            sumplayer(2)
-            print("Игрок "+p2_name+" Выиграл\n")
-            score()
-        else:
-            pass
+    print("Бросает кость  " + p1_name)
+    input()
+    p1_val = randint(1, 6)
+    print("Выпала кость  = " + str(p1_val) + "\n")
+    print("Бросает кость " + p2_name)
+    input()
+    p2_val = randint(1, 6)
+    print("Выпала кость  = " + str(p2_val) + "\n")
+    if p1_val > p2_val:
+        sumplayer(1)
+        print("Игрок " + p1_name + " Выиграл\n")
+        score()
+    elif p1_val == p2_val:
+        print("Ничья\n")
+        score()
+
+    elif p1_val < p2_val:
+        sumplayer(2)
+        print("Игрок " + p2_name + " Выиграл\n")
+        score()
+    else:
+        pass
+
 
 while True:
     print("====================================\n")
